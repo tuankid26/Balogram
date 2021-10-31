495431c4bb7b72040826f069a4d46c083a2dbe98
vietanh2k <vietanha1k31@gmail.com>
2021-10-31 22:05:47 +0700

    update

--- components/Item_Messenger.js
+++ components/Item_Messenger.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, Image ,TouchableOpacity } from 'react-native'
-import Swipeout from 'react-native-swipeout'
-import Icon from 'react-native-vector-icons/FontAwesome5'
+// import Swipeout from 'react-native-swipeout'
+// import Icon from 'react-native-vector-icons/FontAwesome5'
 import { StyleSheet, Dimensions } from 'react-native'
 
 const { width } = Dimensions.get('window')
