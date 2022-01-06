6925c83b6c9347428b80f5c6af128a0665e4968e
Haiduongcable <haiduongqweasd@gmail.com>
2022-01-06 22:32:18 +0700

    Update Icon

--- handle_api/api.js
+++ handle_api/api.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 const axiosClient = axios.create({
     // baseURL: 'http://192.168.1.153:8000/api/v1',
     
-    baseURL: 'http://192.168.1.29:8000/api/v1',
+    // baseURL: 'http://192.168.1.29:8000/api/v1',
     // baseURL: 'http://duongcheckerviet.tk:8000/api/v1',
+    baseURL: "http://150.230.109.159:8000/api/v1",
     responseType: 'json',
     timeout: 10 * 1000,
 });
 
-const SOCKET_URL = "http://duongcheckerviet.tk:3000/api/v1";
+// const SOCKET_URL = "http://duongcheckerviet.tk:3000/api/v1";
+const SOCKET_URL = "http://150.230.109.159:8000/api/v1";
 export default axiosClient;
