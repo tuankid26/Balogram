4b43092c05f244a6a9599cf90812f23bb0ffc625
Haiduongcable <haiduongqweasd@gmail.com>
2022-01-07 19:54:36 +0700

    update admin

--- handle_api/api.js
+++ handle_api/api.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 const axiosClient = axios.create({
     // baseURL: 'http://192.168.1.153:8000/api/v1',
     
-    baseURL: 'http://192.168.1.29:8000/api/v1',
-    // baseURL: 'http://duongcheckerviet.tk:8000/api/v1',
+    // baseURL: 'http://192.168.1.29:8000/api/v1',
+    baseURL: 'http://150.230.98.85:8000/api/v1',
     // baseURL: "http://140.238.58.32:8000/api/v1",
     responseType: 'json',
     timeout: 10 * 1000,
 });
 
-// const SOCKET_URL = "http://duongcheckerviet.tk:3000/api/v1";
-const SOCKET_URL = "http://140.238.58.32:3000/api/v1";
+const SOCKET_URL = "http://150.230.98.85:3000/api/v1";
+// const SOCKET_URL = "http://140.238.58.32:3000/api/v1";
 export default axiosClient;
