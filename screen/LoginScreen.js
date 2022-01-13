cee255e90bf23c7efdf5fc2f281019bb52a4c375
unknown <minhtuantna01@gmail.com>
2022-01-14 00:53:37 +0700

    update chat

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -18,10 +18,10 @@ export default function LoginScreen({ navigation }) {
   const dispatch = useDispatch();
   const onLoginPressed = () => {
     const data = {
-      phonenumber: phonenumber,
-      password: password
-      // phonenumber: "03565184741",
-      // password: "123123123"
+      // phonenumber: phonenumber,
+      // password: password
+      phonenumber: "000001",
+      password: "123123123"
     }
 
     auth.login(data)
