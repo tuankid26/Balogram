d15c4c7211b45fe9a519d533837b23d4d5eb4f44
Haiduongcable <haiduongqweasd@gmail.com>
2022-01-15 21:44:53 +0700

    Update api

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -18,10 +18,10 @@ export default function LoginScreen({ navigation }) {
   const dispatch = useDispatch();
   const onLoginPressed = () => {
     const data = {
-      // phonenumber: phonenumber,
-      // password: password
-      phonenumber: "0356518472",
-      password: "deptrai123"
+      phonenumber: phonenumber,
+      password: password
+      // phonenumber: "0356518472",
+      // password: "deptrai123"
     }
 
     auth.login(data)
